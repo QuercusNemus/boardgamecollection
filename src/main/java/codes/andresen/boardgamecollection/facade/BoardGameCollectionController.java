07ae85caa2417280e4069501acdb05792c519071
Andresen <eric@andresen.codes>
2019-01-23 17:17:45 +0100

    Added Cross Origin support for all endpoints Cleaning

--- src/main/java/codes/andresen/boardgamecollection/facade/BoardGameCollectionController.java
+++ src/main/java/codes/andresen/boardgamecollection/facade/BoardGameCollectionController.java
@@ -3,6 +3,10 @@ package codes.andresen.boardgamecollection.facade;
 import codes.andresen.boardgamecollection.integration.GameCollectionService;
 import codes.andresen.boardgamecollection.model.Game.BoardGame;
 import codes.andresen.boardgamecollection.model.Game.BoardGames;
+import io.netty.handler.codec.Headers;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,12 +22,14 @@ public class BoardGameCollectionController {
         this.gameCollectionService = gameCollectionService;
     }
 
+    @CrossOrigin()
     @PostMapping("/collection/add")
     public StringBuilder addCollection(
             @RequestHeader(value = "userName") String userName) {
         return gameCollectionService.getCollection(userName);
     }
 
+    @CrossOrigin()
     @PostMapping("/collection/add/{gameId}")
     public String addSingleGame(
             @RequestHeader(value = "userName") String userName,
@@ -38,18 +44,21 @@ public class BoardGameCollectionController {
         return gameCollectionService.getGameCollection(userName);
     }
 
+    @CrossOrigin()
     @GetMapping("/search/game")
     public BoardGames getSearchGame(
             @RequestHeader(value = "searchString") String searchString) {
         return gameCollectionService.searchBoardGames(searchString);
     }
 
+    @CrossOrigin()
     @GetMapping("/get/game")
     public BoardGames getGame(
             @RequestHeader(value = "gameId") int gameId) {
         return gameCollectionService.getGame(gameId);
     }
 
+    @CrossOrigin()
     @DeleteMapping(value = "/collection/delete/game/{gameName}")
     public String deleteSingleGame(
             @RequestHeader(value = "userName") String userName,
@@ -57,6 +66,7 @@ public class BoardGameCollectionController {
      return gameCollectionService.deleteSingleGame(userName, gameName);
     }
 
+    @CrossOrigin()
     @DeleteMapping(value = "/collection/delete")
     public String deleteCollection(
             @RequestHeader(value = "userName") String userName) {
